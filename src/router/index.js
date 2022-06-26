13cca09ca121af0cdbc62ac9c572aa7560fc0106
WangYiFan955 <10980145+wangyifan955@user.noreply.gitee.com>
2022-06-26 21:29:43 +0800

    perf: 优化

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,18 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 
 const routes = [{
-    path: '/',
-    name: 'login',
-    component: () =>
-        import ('../views/Login')
-}]
+        path: '/',
+        name: 'login',
+        component: () =>
+            import ('../views/Login')
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () =>
+            import ('../views/profile')
+    }
+]
 
 const router = createRouter({
     history: createWebHashHistory(),
---
+++ src/views/profile/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue'
+</script>
+
+<style lang="scss" scoped></style>
